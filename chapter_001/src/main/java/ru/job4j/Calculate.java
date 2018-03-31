6cc4149e86219edb01b2663b5d259611ebf47200
foxrec123 <foxrec123@mail.ru>
2018-03-31 13:54:10 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
+
 package ru.job4j;
 
+/**
+ * Calculate
+ * 
+ * @author Ivan Vasilyev (foxrec123@mail.ru)
+ */
 public class Calculate {
 	
+	/**
+	 * Main
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+* @author Ivan Vasilyev	(foxrec123@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
