6d47aa49317e389817f27068efcd5dd04e436697
foxrec123 <foxrec123@mail.ru>
2018-04-02 22:58:31 +0300

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,26 +3,27 @@ package ru.job4j;
 
 /**
  * Calculate
- * 
+ *
  * @author Ivan Vasilyev (foxrec123@mail.ru)
  */
 public class Calculate {
-	
-	/**
-	 * Main
-	 * @param args - args.
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-	
-	/**
+
+    /**
+     * Main
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
      * Method echo.
      * @param name Your name.
      * @return Echo plus your name.
      */
     public String echo(String name) {
+
         return "Echo, echo, echo : " + name;
     }
-	
+
 }
